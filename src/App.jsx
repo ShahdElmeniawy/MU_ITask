be29603ad8e762ef0f894c8f3eee57d86bf4f573
ShahdElmeniawy <shahedmohamed180@gmail.com>
2026-04-21 19:58:26 +0200

    update deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { HashRoture, Routes, Route } from "react-router-dom";
+import {Routes, Route, HashRouter} from "react-router-dom";
 import LandingPage from "./LandingPage";
 import FormPage from "./FormPage";
 const App = () => {
   return (
-    <HashRoture>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<FormPage />} />
       </Routes>
-    </HashRoture>
+    </HashRouter>
   );
 };
 
